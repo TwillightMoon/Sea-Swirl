97e013016bff3a20bc101048b70d6f2a4ebcc20a
Lorsenok <121055045+Lorsenok@users.noreply.github.com>
2025-05-26 23:13:55 +0300

    level and sonar fix

--- Assets/_Scripts/Gameplay/Sonar/SonarLine.cs
+++ Assets/_Scripts/Gameplay/Sonar/SonarLine.cs
@@ -7,7 +7,7 @@ public class SonarLine : MonoBehaviour
     [SerializeField] private GameObject radarPoint;
     [SerializeField] private Transform sonar;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (!collision.TryGetComponent(out SonarPoint point)) return;
         GameObject obj = Instantiate(radarPoint, collision.transform.position, collision.transform.rotation, sonar);
